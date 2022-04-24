0875fe4ba0ff4b041d81a9b24d6d41972402c902
logesh3 <logesh@crystaldelta.com>
2022-04-25 09:54:28 +1000

    Basic amplify and amplify auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "smartpicker4198844741988447": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Router, Route } from "react-router-dom";
+import { createBrowserHistory as createHistory } from 'history'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
 
+Amplify.configure(config)
+
+const history = createHistory();
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      <div className="App">
+        <Router history={history}>
+          <Navbar bg="primary" expand="lg" variant="dark" >
+            <Navbar.Brand href="/">Image Gallery App</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/uploadImage">Upload Image</Nav.Link>
+
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          {/* <Route path="/" exact component={HomePage} />
+          <Route path="/uploadImage" exact component={UploadImage} /> */}
+
+        </Router>
+      </div>
+    </Authenticator>
   );
 }
 
