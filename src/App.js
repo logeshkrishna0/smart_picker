5a1887b239274e64a4ca7eed94857463783525af
logesh3 <logesh@crystaldelta.com>
2022-04-26 07:39:21 +1000

    Signup and Login

--- src/App.js
+++ src/App.js
@@ -1,41 +1,51 @@
 import React from 'react';
-import { Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes, //replaces "Switch" used till v5
+  Route,
+} from "react-router-dom";
 import { createBrowserHistory as createHistory } from 'history'
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import './App.css';
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
+import './App.css';
+import HomePage from './Components/Homepage';
+import UploadImage from './Components/uploadImage';
 import Amplify from 'aws-amplify'
 import config from './aws-exports'
 
 Amplify.configure(config)
 
 const history = createHistory();
+
 function App() {
-
   return (
-    <Authenticator>
-      <div className="App">
-        <Router history={history}>
-          <Navbar bg="primary" expand="lg" variant="dark" >
-            <Navbar.Brand href="/">Image Gallery App</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/uploadImage">Upload Image</Nav.Link>
+    <div className="App">
+      <Authenticator>
+        {({ signOut, user }) => (
+          <Router history={history}>
+            <Navbar bg="primary" expand="lg" variant="dark" >
+              <Navbar.Brand href="/">Image Gallery App</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <Nav.Link href="/uploadImage">Upload Image</Nav.Link>
+                  <button onClick={signOut}>Sign out</button>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/uploadImage" element={<UploadImage />} />
+            </Routes>
+          </Router>
+        )}
+      </Authenticator>
+    </div>
 
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
 
-          {/* <Route path="/" exact component={HomePage} />
-          <Route path="/uploadImage" exact component={UploadImage} /> */}
-
-        </Router>
-      </div>
-    </Authenticator>
   );
 }
 
