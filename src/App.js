d3677a62a9cf4f7863281bdd0f95050b4310de91
logesh3 <logesh@crystaldelta.com>
2022-04-26 07:52:33 +1000

    Amplify resetup

--- src/App.js
+++ src/App.js
@@ -22,27 +22,29 @@ const history = createHistory();
 function App() {
   return (
     <div className="App">
-      <Authenticator>
-        {({ signOut, user }) => (
-          <Router history={history}>
-            <Navbar bg="primary" expand="lg" variant="dark" >
-              <Navbar.Brand href="/">Image Gallery App</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                  <Nav.Link href="/">Home</Nav.Link>
-                  <Nav.Link href="/uploadImage">Upload Image</Nav.Link>
-                  <button onClick={signOut}>Sign out</button>
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/uploadImage" element={<UploadImage />} />
-            </Routes>
-          </Router>
-        )}
-      </Authenticator>
+      <div className='centered'>
+        <Authenticator>
+          {({ signOut, user }) => (
+            <Router history={history}>
+              <Navbar bg="primary" expand="lg" variant="dark" >
+                <Navbar.Brand href="/">Image Gallery App</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/uploadImage">Upload Image</Nav.Link>
+                    <button onClick={signOut}>Sign out</button>
+                  </Nav>
+                </Navbar.Collapse>
+              </Navbar>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/uploadImage" element={<UploadImage />} />
+              </Routes>
+            </Router>
+          )}
+        </Authenticator>
+      </div>
     </div>
 
 
